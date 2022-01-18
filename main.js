d760f2fe4fb6eecc3bfbfedeb3cca4f0c7aa15a6
coding-to-music <connors.tom@gmail.com>
2022-01-18 05:04:54 +0000

    build

--- main.js
+++ main.js
@@ -5,21 +5,109 @@ const request = require("request");
 const filter = require("through2-filter");
 const sentiment = require("sentiment");
 const util = require("util");
+const dotenv = require('dotenv').config()
 const pipeline = util.promisify(stream.pipeline);
-const mongoose = require("mongoose");
 const { MongoClient } = require("mongodb");
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraperData"
 
+var timetaken = "Time taken by addCount function";
 console.log("MONGODB_URI", MONGODB_URI);
 
-// mongoose.Promise = Promise;
+var timetaken = "Time taken by addCount function";
 
-mongoose.connect(MONGODB_URI, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    // useCreateIndex: true
-  });
+// console.time("loop");
+
+console.time(timetaken);
+
+(async () => {
+  const client = new MongoClient(process.env["MONGODB_URI"], { useUnifiedTopology: true });
+  const textRank = new sentiment();
+  try {
+      await client.connect();
+      const collection = client.db("hacker-news").collection("mentions");
+      await pipeline(
+          request("http://api.hnstream.com/comments/stream/"),
+          ndjson.parse({ strict: false }),
+          // filter({ objectMode: true }, chunk => {
+          //     return chunk["body"].toLowerCase().includes("bitcoin") || chunk["article-title"].toLowerCase().includes("bitcoin");
+          // }),
+          through2.obj((row, enc, next) => {
+              let result = textRank.analyze(row.body);
+              console.log("result.score", result.score);
+              row.score = result.score;
+              next(null, row);
+          }),
+          through2.obj((row, enc, next) => {
+            console.log("insertOne");
+            collection.insertOne({
+                  ...row,
+                  "user-url": `https://news.ycombinator.com/user?id=${row["author"]}`,
+                  "item-url": `https://news.ycombinator.com/item?id=${row["article-id"]}`
+              });
+              next();
+          })
+      );
+      console.timeEnd(timetaken);
+      console.log("FINISHED");
+  } catch(error) {
+      console.log(error);
+  }
+})();
+
+
+// (async () => {
+//   const client = new MongoClient(process.env["MONGODB_URI"], { useUnifiedTopology: true });
+//   const textRank = new sentiment();
+//   try {
+//       await client.connect();
+//       const collection = client.db("hacker-news").collection("mentions");
+//       await pipeline(
+//           request("http://api.hnstream.com/comments/stream/"),
+//           ndjson.parse({ strict: false }),
+//           filter({ objectMode: true }, chunk => {
+//               return chunk["body"].toLowerCase().includes("bitcoin") || chunk["article-title"].toLowerCase().includes("bitcoin");
+//           }),
+//           through2.obj((row, enc, next) => {
+//               let result = textRank.analyze(row.body);
+//               row.score = result.score;
+//               next(null, row);
+//           })
+//       );
+//       console.timeEnd(timetaken);
+//       console.log("FINISHED");
+//   } catch(error) {
+//       console.log(error);
+//   }
+// })();
+
+
+// (async () => {
+//   const client = new MongoClient(process.env["MONGODB_URI"], { useUnifiedTopology: true });
+//   const textRank = new sentiment();
+//   try {
+//       await client.connect();
+//       const collection = client.db("hacker-news").collection("mentions");
+//       await pipeline(
+//           request("http://api.hnstream.com/comments/stream/"),
+//           ndjson.parse({ strict: false }),
+//           filter({ objectMode: true }, chunk => {
+//               return chunk["body"].toLowerCase().includes("bitcoin") || chunk["article-title"].toLowerCase().includes("bitcoin");
+//           }),
+//           through2.obj((row, enc, next) => {
+//               let result = textRank.analyze(row.body);
+//               row.score = result.score;
+//               next(null, row);
+//           })
+//       );
+//       console.timeEnd(timetaken);
+//       console.log("FINISHED");
+//   } catch(error) {
+//       console.log(error);
+//   }
+// })();
+
+// console.timeEnd();
 
 // (async () => {
 //     const client = new MongoClient(process.env["MONGODB_URI"], { useUnifiedTopology: true });
@@ -31,3 +119,25 @@ mongoose.connect(MONGODB_URI, {
 //         console.log(error);
 //     }
 // })();
+
+// (async () => {
+//   const client = new MongoClient(process.env["MONGODB_URI"], { useUnifiedTopology: true });
+//   console.log("About to try");
+//   try {
+//       await client.connect();
+//       const collection = client.db("hacker-news").collection("mentions");
+//       console.log("About to await pipeline");
+//       await pipeline(
+//           request("http://api.hnstream.com/comments/stream/"),
+//           ndjson.parse({ strict: false }),
+//           filter({ objectMode: true }, chunk => {
+//               return chunk["body"].toLowerCase().includes("bitcoin") || chunk["article-title"].toLowerCase().includes("bitcoin");
+//           })
+//       );
+//       console.log("FINISHED");
+//   } catch(error) {
+//       console.log(error);
+//   }
+// })();
+
+
